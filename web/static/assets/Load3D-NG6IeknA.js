e5d2503c54bf3bfefc4683871042b6e8c73a1d03
pixelmaven <pixelmavenai@gmail.com>
2026-04-08 14:46:34 +0400

    Add custom web frontend with title and favicon

---
+++ web/static/assets/Load3D-NG6IeknA.js
@@ -0,0 +1,2 @@
+import{_ as o}from"./Load3D.vue_vue_type_script_setup_true_lang-DYT5Ecka.js";import"./vendor-other-B8t_Wf2K.js";import"./vendor-primevue-DeKsC2uk.js";import"./index-BFHZsBkN.js";import"./vendor-vue-DaLZjKA7.js";import"./vendor-xterm-CWYFmgbN.js";import"./vendor-three-DH0o8VQ7.js";import"./vendor-tiptap-VxK0yxkd.js";export{o as default};
+//# sourceMappingURL=Load3D-NG6IeknA.js.map
---
+++ web/static/assets/WidgetLegacy-DxWXa1q8.js
@@ -0,0 +1,2 @@
+import{a$ as o}from"./index-BFHZsBkN.js";import"./vendor-primevue-DeKsC2uk.js";import"./vendor-other-B8t_Wf2K.js";import"./vendor-vue-DaLZjKA7.js";import"./vendor-xterm-CWYFmgbN.js";import"./vendor-three-DH0o8VQ7.js";import"./vendor-tiptap-VxK0yxkd.js";export{o as default};
+//# sourceMappingURL=WidgetLegacy-DxWXa1q8.js.map
